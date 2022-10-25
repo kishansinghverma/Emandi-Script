e133d4dd170ae7a167c4f4369968ae9ef7879bb8
kishansinghverma <46574301+kishansinghverma@users.noreply.github.com>
2022-10-26 01:56:54 +0530

    Create EmandiScriptLoader.js

---
+++ EmandiScriptLoader.js
@@ -0,0 +1,5 @@
+var script = document.createElement('script');
+script.innerHTML = `import {RunScript} from 'https://cdn.jsdelivr.net/gh/kishansinghverma/GlobalResources/EmandiTest.js';
+RunScript();`;
+script.setAttribute('type', 'module');
+document.getElementsByTagName('body')[0].appendChild(script);
