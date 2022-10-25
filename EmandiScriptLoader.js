cd31d792e3989091091d3ac31acff1698807a355
kishansinghverma <46574301+kishansinghverma@users.noreply.github.com>
2022-10-26 02:02:54 +0530

    Update EmandiScriptLoader.js

--- EmandiScriptLoader.js
+++ EmandiScriptLoader.js
@@ -1,5 +1,5 @@
 var script = document.createElement('script');
-script.innerHTML = `import {RunScript} from 'https://cdn.jsdelivr.net/gh/kishansinghverma/GlobalResources/EmandiTest.js';
+script.innerHTML = `import {RunScript} from 'https://cdn.jsdelivr.net/gh/kishansinghverma/GlobalResources@main/EmandiTest.js';
 RunScript();`;
 script.setAttribute('type', 'module');
 document.getElementsByTagName('body')[0].appendChild(script);
