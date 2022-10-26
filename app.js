ed8831747f393d1ab42fc7c924732e12d01f3543
DIR\kishan.e.singh <kishan.e.singh@accenture.com>
2022-10-26 09:41:18 +0530

    Route working

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+var cors = require('cors')
 const port = process.env.PORT || 3001;
 
+app.use(cors())
 app.use('/static', express.static('static'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
