6076404e554596978b14fc91f1d13bca1b4bb66f
DIR\kishan.e.singh <kishan.e.singh@accenture.com>
2023-01-13 13:37:41 +0530

    ..

--- static/EmandiScriptAsModule.js
+++ static/EmandiScriptAsModule.js
@@ -1,5 +1,5 @@
 var script = document.createElement('script');
-script.innerHTML = `import {RunScript} from 'https://cdn.jsdelivr.net/gh/kishansinghverma/Emandi-Script@daf5d2d/static/Emandi.js';
+script.innerHTML = `import {RunScript} from 'https://cdn.jsdelivr.net/gh/kishansinghverma/Emandi-Script@675b85d/static/Emandi.js';
 RunScript();`;
 script.setAttribute('type', 'module');
 document.getElementsByTagName('body')[0].appendChild(script);
