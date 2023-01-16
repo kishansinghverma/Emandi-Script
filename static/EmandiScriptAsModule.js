b6760f0b7caa56368ef36d34cc8cc34253ed6866
DIR\kishan.e.singh <kishan.e.singh@accenture.com>
2023-01-16 19:23:37 +0530

    ..

--- static/EmandiScriptAsModule.js
+++ static/EmandiScriptAsModule.js
@@ -1,4 +1,17 @@
-var script = document.createElement('script');
-script.setAttribute('type', 'module');
-script.innerHTML = "import { RunScript } from 'https://cdn.jsdelivr.net/gh/kishansinghverma/Emandi-Script@bdeb0aa/static/Emandi.js'; RunScript();";
-document.getElementsByTagName('body')[0].appendChild(script);
+// ==UserScript==
+// @name         EMandi Helper
+// @version      1.0
+// @description  Helper Script to Autofill EMandi
+// @author       Kishan
+// @match        https://emandi.up.gov.in/*
+// @grant        none
+
+// ==/UserScript==
+
+window.addEventListener('load', (event) => {
+    console.log("Injecting Emandi Script...");
+    var script = document.createElement('script');
+    script.setAttribute('type', 'module');
+    script.innerHTML = "import { RunScript } from 'https://cdn.jsdelivr.net/gh/kishansinghverma/Emandi-Script@bdeb0aa/static/Emandi.js'; RunScript();";
+    document.getElementsByTagName('body')[0].appendChild(script);
+});
