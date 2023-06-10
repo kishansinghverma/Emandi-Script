9064c0a6c0826026bd1b4b40d672e9f916ed8d44
kishansinghverma <kishansinghverma@gmail.com>
2023-06-11 01:41:58 +0530

    ..

--- other/ScriptInjection.js
+++ other/ScriptInjection.js
@@ -1,3 +1,23 @@
+// For Phone
+// ==UserScript==
+// @name         EMandi Helper
+// @version      1.0
+// @description  Helper Script to Autofill EMandi
+// @author       Kishan
+// @match        https://emandi.up.gov.in/*
+// @grant        none
+// @run-at document-end
+
+// ==/UserScript==
+window.addEventListener('load', (event) => {
+    console.log("Injecting Emandi Script...");
+    var script = document.createElement('script');
+    script.setAttribute('type', 'module');
+    script.src = "https://cdn.jsdelivr.net/gh/kishansinghverma/Emandi-Script@main/dist/emandi.js";
+    document.getElementsByTagName('body')[0].appendChild(script);
+})
+
+// For Windows
 // ==UserScript==
 // @name         EMandi Helper
 // @version      1.0
@@ -11,6 +31,6 @@
 console.log("Injecting Emandi Script...");
 var script = document.createElement('script');
 script.setAttribute('type', 'module');
-script.src = "http://localhost:3001/public/Emandi.js";
-//script.src = "https://cdn.jsdelivr.net/gh/kishansinghverma/Emandi-Script@main/dist/emandi.js";
+//script.src = "http://localhost:3001/public/Emandi.js";
+script.src = "https://cdn.jsdelivr.net/gh/kishansinghverma/Emandi-Script@main/dist/emandi.js";
 document.getElementsByTagName('body')[0].appendChild(script);
