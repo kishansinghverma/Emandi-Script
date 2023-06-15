e60b900565f505a09c09e638e5ef5cd39e48e3be
kishansinghverma <kishansinghverma@gmail.com>
2023-06-15 12:47:47 +0530

    Fixing Captcha Reload

--- src/public/modules/login.js
+++ src/public/modules/login.js
@@ -6,13 +6,35 @@ import { ResolveCaptcha, SetResolvedCaptcha, ShowAlert } from "./utils.js";
 class LoginForm extends Form {
     InitializeForm = () => {
         ShowAlert(MessageType.Info, 'Logging In, Please Wait...', 5);
+        this.ParseCaptcha();
+        this.RegisterListeners();
+    }
+
+    RegisterListeners() {
+        const targetNode = document.querySelector('form > div:nth-child(3) .col-sm-10');
+        const config = { childList: true };
+        const callback = (mutationList, observer) => {
+            for (const mutation of mutationList) {
+                this.ParseCaptcha();
+            }
+        };
+
+        const observer = new MutationObserver(callback);
+        observer.observe(targetNode, config);
+    }
+
+    ParseCaptcha() {
+        this.Source = $('#dntCaptchaImg').attr('src');
         $('#userid').val("Kishanverma.guest@gmail.com");
         $('#pwd').val("Kishan@123");
-        
+
         ResolveCaptcha('dntCaptchaImg')
             .then(text => {
-                SetResolvedCaptcha(text, 'DNTCaptchaInputText');
-                $('#btnsubmit').click();
+                if (this.Source === $('#dntCaptchaImg').attr('src')) {
+                    SetResolvedCaptcha(text, 'DNTCaptchaInputText');
+                    $('#btnsubmit').click();
+                }
+                else this.ParseCaptcha();
             })
             .catch(AlertError);
     }
