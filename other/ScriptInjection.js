94e3ca37c4d2718a63765659ecfb8803a798af10
kishansinghverma <kishansinghverma@gmail.com>
2023-06-26 14:24:11 +0530

    ..

--- other/ScriptInjection.js
+++ other/ScriptInjection.js
@@ -1,36 +1,20 @@
-// For Phone
 // ==UserScript==
 // @name         EMandi Helper
 // @version      1.0
 // @description  Helper Script to Autofill EMandi
 // @author       Kishan
 // @match        https://emandi.up.gov.in/*
+// @match        http://emandi.up.gov.in/*
 // @grant        none
-// @run-at document-end
+// @run-at       document-start
 
 // ==/UserScript==
-window.addEventListener('load', (event) => {
+
+window.addEventListener('load', () => {
     console.log("Injecting Emandi Script...");
-    var script = document.createElement('script');
+    const script = document.createElement('script');
     script.setAttribute('type', 'module');
+    //script.src = "http://localhost:3001/public/Emandi.js";
     script.src = "https://cdn.jsdelivr.net/gh/kishansinghverma/Emandi-Script@main/dist/emandi.js";
-    document.getElementsByTagName('body')[0].appendChild(script);
-})
-
-// For Windows
-// ==UserScript==
-// @name         EMandi Helper
-// @version      1.0
-// @description  Helper Script to Autofill EMandi
-// @author       Kishan
-// @match        https://emandi.up.gov.in/*
-// @grant        none
-// @run-at document-end
-
-// ==/UserScript==
-console.log("Injecting Emandi Script...");
-var script = document.createElement('script');
-script.setAttribute('type', 'module');
-//script.src = "http://localhost:3001/public/Emandi.js";
-script.src = "https://cdn.jsdelivr.net/gh/kishansinghverma/Emandi-Script@main/dist/emandi.js";
-document.getElementsByTagName('body')[0].appendChild(script);
+    document.body.appendChild(script);
+});
