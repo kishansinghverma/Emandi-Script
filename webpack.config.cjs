13013d44678fb1d2bdc4869c915d94f1aa3ca278
kishansinghverma <kishansinghverma@gmail.com>
2024-04-21 18:11:21 +0530

    Fixing loaders

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,10 +1,19 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     mode: 'production',
     entry: './src/public/index.js',
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, './dist'),
+        path: path.resolve('./dist'),
     },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: './src/public/assets/style.css', to: './assets/style.css' },
+                { from: './other/scriptloader.js', to: './scriptloader.js' }
+            ],
+        }),
+    ],
 };
