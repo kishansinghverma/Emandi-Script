7c8b0bb3c59f54a8f9d194f579b099045b30a495
kishansinghverma <kishansinghverma@gmail.com>
2024-04-21 18:45:11 +0530

    ..

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,5 +1,4 @@
 const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     mode: 'production',
@@ -7,13 +6,5 @@ module.exports = {
     output: {
         filename: 'index.js',
         path: path.resolve('./dist'),
-    },
-    plugins: [
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: './src/public/assets/style.css', to: './assets/style.css' },
-                { from: './other/scriptloader.js', to: './scriptloader.js' }
-            ],
-        }),
-    ],
+    }
 };
