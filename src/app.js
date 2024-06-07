4e9ba8ec5b2187c46daf3e630d31c98b33a7ce7b
kishansinghverma <kishansinghverma@gmail.com>
2024-06-07 22:40:40 +0530

    ..

--- src/app.js
+++ src/app.js
@@ -7,3 +7,5 @@ app.use(cors())
 app.use('/public', express.static(path.join(resolve(), 'src', 'public')));
 
 const port = process.env.PORT || 3001;
+
+app.listen(port, () => console.log(`Proxy server running on port ${port}!`));
