db2f767ad7c0b65256857d3cb17054dfbbeae5ce
kishansinghverma <kishansinghverma@gmail.com>
2025-05-10 14:41:02 +0530

    Re:

--- src/public/modules/pages/login.js
+++ src/public/modules/pages/login.js
@@ -26,7 +26,7 @@ class LoginForm {
     }
 
     executeInitialActions() {
-        showAlert(MessageType.Success, 'Script Injection Ready...')
+        showAlert(MessageType.Success, 'Script Ready...')
         $('#dntCaptchaRefreshButton').click();
         this.loginPromise.resolve();
     }
